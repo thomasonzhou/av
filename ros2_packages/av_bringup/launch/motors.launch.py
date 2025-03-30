8a4f9ae200c15c53c1c8576f164f572867faf579
Thomason Zhou <t95zhou@uwaterloo.ca>
2025-03-30 20:56:40 +0900

    Add bringup for motors

---
+++ ros2_packages/av_bringup/launch/motors.launch.py
@@ -0,0 +1,38 @@
+import os
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+TEST_MOTOR_ID   = 0x7D
+LEFT_MOTOR_ID   = 0x7E
+RIGHT_MOTOR_ID  = 0x7F
+
+def generate_launch_description():
+    nodes = []
+    
+    nodes.append(Node(
+        package='cybergear_socketcan_driver',
+        executable='cybergear_velocity_driver_node',
+        name='cybergear_right',
+        parameters=[{'device_id': RIGHT_MOTOR_ID}, {'joint_name': 'right_wheel_joint'}]
+    ))
+
+    nodes.append(Node(
+        package='cybergear_socketcan_driver',
+        executable='cybergear_velocity_driver_node',
+        name='cybergear_left',  
+        parameters=[{'device_id': LEFT_MOTOR_ID}, {'joint_name': 'left_wheel_joint'}]
+    ))
+
+    socketcan_bridge_launch = IncludeLaunchDescription(
+        XMLLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('ros2_socketcan'), 'launch/socket_can_bridge.launch.xml')
+        )
+    )
+
+    return LaunchDescription([
+        *nodes,
+        socketcan_bridge_launch
+    ])
